41f3f0d027da26d924ecdff70df00a75c6767f2f
Kenneth Torng <ktorng@gmail.com>
2017-10-31 15:11:00 -0500

    GH-15 Write unit tests for game utils

--- src/utils/game.js
+++ src/utils/game.js
@@ -1,18 +1,25 @@
-// checks if game is valid and all players are ready
+/**
+ * Checks if game is valid and all players are ready
+ *
+ * @param  {Array}  playerList
+ * @return {boolean}
+ */
 export const isGameReady = playerList => {
-  // if any players are unassigned
+  // If any players are unassigned
   if (playerList.some(player => !player.team)) return false;
 
-  // check if team has spymaster and at least one operative
+  // Check if team has spymaster and at least one operative
   const teams = {};
-  const playersReady = playerList.every(player => {
+
+  playerList.forEach(player => {
     if (!teams[player.team]) teams[player.team] = {};
     teams[player.team][player.role] = true;
-    return player.status === 'ready';
   });
+
   const validTeam =
     (teams.Red && teams.Red.Spymaster && teams.Red.Operative) ||
     (teams.Blue && teams.Blue.Spymaster && teams.Blue.Operative);
+  const playersReady = playerList.every(player => player.status === 'ready');
 
   return playersReady && validTeam;
 };
