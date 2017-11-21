26cff06a53a1566ec8d3f6c0938da67ca6fc4dd4
Kenneth Torng <ktorng@gmail.com>
2017-11-20 19:34:46 -0600

    GH-25 Fix lobby layout flex direction

--- src/common/layout/LobbyGrid.js
+++ src/common/layout/LobbyGrid.js
@@ -14,34 +14,33 @@ const styles = () => ({
 });
 
 const LobbyGrid = props => (
-  <Grid container className={props.classes.root}>
-    <Grid item container direction="column" className={props.classes.grow}>
-      <Grid
-        container
-        className={props.classes.grow}
-        direction="column"
-        alignItems="center"
-        justify="center"
-      >
-        <Grid item xs={12}>
-          {props.headerComponent}
+  <Grid
+    container
+    className={props.classes.root}
+    alignItems="center"
+    justify="center"
+  >
+    <Grid item xs={12}>
+      <Grid container alignItems="center" justify="center" spacing={16}>
+        <Grid item>{props.headerComponent}</Grid>
+      </Grid>
+    </Grid>
+    <Grid item xs={12}>
+      <Grid container alignItems="center" justify="center" spacing={16}>
+        <Grid item xs={4}>
+          {props.redComponent}
         </Grid>
-        <Grid item xs={12}>
-          <Grid container alignItems="center" justify="center" spacing={16}>
-            <Grid item xs={4}>
-              {props.redComponent}
-            </Grid>
-            <Grid item xs={2}>
-              {props.unassignedComponent}
-            </Grid>
-            <Grid item xs={4}>
-              {props.blueComponent}
-            </Grid>
-          </Grid>
+        <Grid item xs={3}>
+          {props.unassignedComponent}
         </Grid>
-        <Grid item xs={12}>
-          {props.optionsComponent}
+        <Grid item xs={4}>
+          {props.blueComponent}
         </Grid>
+      </Grid>
+    </Grid>
+    <Grid item xs={12}>
+      <Grid container alignItems="center" justify="center" spacing={16}>
+        <Grid item>{props.optionsComponent}</Grid>
       </Grid>
     </Grid>
   </Grid>
